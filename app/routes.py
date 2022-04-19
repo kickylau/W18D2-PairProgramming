9ce188205a3c7fc6fbd5a47a015f6fb84dc6a262
kickylau <kickyliu2018@gmail.com>
2022-04-19 19:36:08 -0400

    table

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,26 @@
+import os
+import psycopg2
 from flask import (Blueprint, render_template)
 
 
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
+CONNECTION_PARAMETERS = {
+    "user": os.environ.get("DB_USER"),
+    "password": os.environ.get("DB_PASS"),
+    "dbname": os.environ.get("DB_NAME"),
+    "host": os.environ.get("DB_HOST"),
+}
+
 @bp.route("/")
 def main():
-    return render_template("main.html")
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor() as curs:
+            curs.execute("SELECT id, name, start_datetime, end_datetime FROM appointments ORDER BY start_datetime;")
+            result = curs.fetchall()
+            #print(result)
+            return render_template("main.html", rows=rows)
 
+
+print(main())
