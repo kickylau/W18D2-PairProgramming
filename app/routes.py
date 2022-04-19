bc41454a6a6e64d9f8c398349280cf8836d7f8dc
arianna <arianna.n.robinson@gmail.com>
2022-04-19 18:21:36 -0400

    changes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
-from flask import Blueprint
+from flask import (Blueprint, render_template)
 
-bp = Blueprint('admin', __name__, url_prefix='/admin')
+
+
+bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route("/")
 def main():
-    return "Calendar working"
+    return render_template("main.html")
+
